2f8fcd0e3cfe068a9edd6cf4fd01074f7290fd78
clstanton <cstanton77@gmail.com>
2020-04-26 09:05:00 -0500

    call api functions with buttons

--- assets/js/homepage.js
+++ assets/js/homepage.js
@@ -1,3 +1,4 @@
+var languageButtonsEl = document.querySelector("#language-buttons")
 var userFormEl = document.querySelector("#user-form");
 var nameInputEl = document.querySelector("#username");
 var repoContainerEl = document.querySelector("#repos-container");
@@ -91,3 +92,30 @@ var displayRepos = function(repos, searchTerm) {
     console.log(repos);
     console.log(searchTerm);
   };
+
+var getFeaturedRepos = function(language) {
+    var apiUrl = "https://api.github.com/search/repositories?q=" + language + "+is:featured&sort=help-wanted-issues";
+  
+    fetch(apiUrl).then(function(response) {
+      if (response.ok) {
+        response.json().then(function(data) {
+          displayRepos(data.items, language);
+        });
+      } else {
+        alert("Error: " + response.statusText);
+      }
+    });
+  };
+  
+  var buttonClickHandler = function(event) {
+    var language = event.target.getAttribute("data-language");
+    if (language) {
+        getFeaturedRepos(language);
+      
+        // clear old content
+        repoContainerEl.textContent = "";
+      }
+    console.log(language);
+  };
+
+  languageButtonsEl.addEventListener("click", buttonClickHandler);
